28b81975905fbebf9fa52bbb6f609a55e1821437
O-Kaiser <oceane.kaiser99@gmail.com>
2020-02-19 16:54:49 +0100

    MAJ TP4

--- TP4/partie_1.c
+++ TP4/partie_1.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 void EncryptLigne1(char* T);
 void EncryptLigne2(char* T, int dec);
@@ -8,61 +9,96 @@ void LUT(char*cle, char* lut);
 void Affiche_LUT(char* cle, char* lut);
 void EncryptLigne3(char* T, char* lut);
 void EncryptFile(FILE* fichier_in, FILE* fichier_out,int methode, char* lut,int taille_buffer);
+void DecryptFile(FILE* fichier_in, FILE* fichier_out,int methode, int taille_buffer,char *lut);
+void DecryptLigne3(char* buffer, char* lut);
 
 int main(void)
 {
-	char T[100];
-	int dec;
-	char cle[]="zephir";
+	int choix,methode,d;
+	int taille_buffer = 1000;
+	char cle[] ="zephir";
 	char lut[27]={'\0'};
-	int methode;
+	char fichier_i[255], fichier_o[255];
 	FILE* fichier_in;
-       	FILE* fichier_out;
-	int taille_buffer=255;
-	int d;
+        FILE* fichier_out;
+	char chemin_in[2000] = "/home/kaiser/Documents/Algo_C/";
+	char chemin_out[2000] = "/home/kaiser/Documents/Algo_C/";
 
-	printf("Entrez une phrase à coder :\n");
-	scanf("%[^\n]s",T);					//[^\n] pour prendre en compte les espaces
-	printf("Choisir le paramètre de décalage\n");
-	do{
-		scanf("%d",&dec);
-	}while(dec < 1 || dec > 26);
+	LUT(cle, lut);
 
-	//Exercice 1
-	EncryptLigne1(T);					
-	printf("Phrase chiffrée avec code César (dec = 13) : %s\n",T);
 
-	//Exercice 2
-	EncryptLigne1(T);			//Décrypté revient à recrypté grâce à la symétrie
-	printf("Phrase déchiffrée avec code César(dec = 13) : %s\n",T);
+printf("Voulez-vous chiffré ou déchiffré : \n 1 - Chiffré\n 2 - Déchiffré \n");
+scanf("%d", &choix);
+while((d = getchar()) != '\n' && d != EOF);
 
-	//Exercice 3
-	EncryptLigne2(T,dec);
-	printf("Phrase chiffrée avec un décalage de %d : %s \n",dec,T);
+switch(choix){
 
-	//Exercice 4
-	DecryptLigne2(T,dec);
-	printf("Phrase déchiffrée avec un décalage de %d : %s\n",dec,T);
+		case 1 : printf("Quel fichier voulez-vous chiffré ? \n");
+			 scanf("%s", fichier_i);
+ 			  while((d = getchar()) != '\n' && d != EOF);
+			 strcat(chemin_in,fichier_i);
+			
+			 if ((fichier_in = fopen(chemin_in, "r")) == NULL){
+				 printf("Attention fichier d'entré vide.\n");
+				 return -1;
+			 }
+
+			 printf("Dans quel fichier voulez-vous afficher le résultat ? \n");
+			 scanf("%s", fichier_o);
+			 while((d = getchar()) != '\n' && d != EOF);
+
+			printf("%s\n",chemin_out);
+
+			 strcat(chemin_out,fichier_o);
+
+			printf("%s\n",chemin_out);
+
+			 fichier_out = fopen(chemin_out, "w");
+
+
+
+			 printf("\nPar quelle méthode voulez-vous crypter votre fichier?\n 1-César (décalage de 13)\n 2-Décalage libre \n 3-Substitution avec clé de chiffrement\n");
+			 scanf("%d",&methode);		      
+		         while((d = getchar()) != '\n' && d != EOF);
+
+		      	 EncryptFile(fichier_in,fichier_out,methode,lut,taille_buffer);
+
+			 break;
+
+		case 2 : printf("Quel fichier voulez-vous déchiffré ? \n");
+			 scanf("%s", fichier_i);
+			 while((d = getchar()) != '\n' && d != EOF);
+			 strcat(chemin_in,fichier_i);
+			 
+			 if((fichier_in = fopen(chemin_in,"r")) == NULL){
+				 printf("Attention fichier d'entré vide.\n");
+				 return -1;
+			}
+
+			 
+			 printf("Dans quel fichier voulez-vous afficher le résultat ?\n");
+			 scanf("%s", fichier_o);
+			 while((d = getchar()) != '\n'  && d != EOF);
+			 strcat(chemin_out,fichier_o);
+
+
+			 fichier_out = fopen(chemin_out,"w");
+
+			 
+			 printf("\nPar quelle méthode voulez-vous crypter votre fichier?\n 1-César (décalage de 13)\n 2-Décalage libre \n 3-Substitution avec clé de chiffrement\n");
+			 scanf("%d",&methode);
+		         while((d = getchar()) != '\n' && d != EOF);
+			 DecryptFile(fichier_in, fichier_out, methode, taille_buffer, lut);
+			 break;
+
+		default : break;
+}
+
+
 	
-	//Exercice 5
-	LUT(cle,lut);
-	Affiche_LUT(cle,lut);
-
-	//Exercice 6
-	EncryptLigne3(T, lut);
-	printf("Phrase chyffrée avec LUT(zephir) : %s\n",T);
-
-
-	//Exercice 7
-	fichier_in = fopen("/home/kaiser/Documents/Algo_C/fichier_in","r");
-	fichier_out = fopen("/home/kaiser/Documents/Algo_C/fichier_out.txt","w");
-	printf("\nPar quelle méthode voulez-vous crypter votre fichier?\n 1-César (décalage de 13)\n 2-Décalage libre \n 3-Substitution avec clé de chiffrement\n");
-	
-	scanf("%d",&methode);
-	while((d = getchar()) != '\n' && d != EOF);
-	EncryptFile(fichier_in,fichier_out,methode,lut,taille_buffer);
 	return 0 ;
 }
+
 
 
 //---------------------------------------------------------------------------------------------------
@@ -158,7 +194,7 @@ void EncryptLigne3(char* T, char* lut)
 void EncryptFile(FILE* fichier_in, FILE* fichier_out, int methode,char* lut, int taille_buffer)
 {
 	int dec;
-	char buffer[taille_buffer]; //Nombre de caractère maximum dans une ligne du fichier
+	char buffer[1000]; //Nombre de caractère maximum dans une ligne du fichier
 
 	switch(methode){
 		case 1 :  while(fgets(buffer, taille_buffer,fichier_in) != NULL){
@@ -194,3 +230,60 @@ void EncryptFile(FILE* fichier_in, FILE* fichier_out, int methode,char* lut, int
 		default : break;
 	}
 }
+
+//Exercice 8
+
+
+void DecryptLigne3(char* buffer, char* lut)
+{
+
+	int i,j;
+	char Alpha[]="abcdefghijklmnopqrstuvwxyz";
+
+	for( i = 0 ; buffer[i] != '\0' ; i++)
+	{
+		for( j = 0 ; buffer[i] != lut[j] ; j++);
+		buffer[i] = Alpha[j];
+
+}
+}
+
+void DecryptFile(FILE* fichier_in, FILE* fichier_out, int methode,int taille_buffer,char* lut)
+{
+	int dec;
+	char buffer[1000];
+
+
+	switch(methode){
+		case 1 : while(fgets(buffer, taille_buffer, fichier_in) != NULL){
+				 EncryptLigne1(buffer);
+				 fprintf(fichier_out,"%s",buffer);
+			}
+			fclose(fichier_out);
+			printf("Fichier déchiffré avec la méthode César : %s\n", buffer);
+			break;
+
+		case 2 :printf("Choisir le paramètre de décalage du décryptage : \n");
+			do{
+				scanf("%d", &dec);
+			}while(dec < 1 || dec > 26);
+			
+			while(fgets(buffer,taille_buffer, fichier_in) != NULL){
+				DecryptLigne2(buffer,dec);
+				fprintf(fichier_out,"%s", buffer);
+			}
+			fclose(fichier_out);
+			printf("FIchier déchiffré avec la méthode de d&cakfe : %s , décalage = %d\n",buffer, dec);
+		case 3 : while(fgets(buffer,taille_buffer,fichier_in) != NULL){
+				 DecryptLigne3(buffer,lut);
+				 fprintf(fichier_out, "%s", buffer);
+			}
+			fclose(fichier_out);
+			printf("Fichier déchiffré par subtsitution (zephir) :%s\n", buffer);
+			break;
+
+
+		default : break;
+}
+}
+
